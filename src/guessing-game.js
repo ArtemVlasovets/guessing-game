a274fae827af7cfe2ad2e198fc479f41a9d8a853
Artem Vlasovets <wlasowez.artem@mail.ru>
2017-02-01 17:53:49 +0300

    guessing-game/fix: binary search

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.array = [];
+      this.left_limit = undefined;
+      this.right_limit = undefined;
+    }
 
     setRange(min, max) {
-
+      for (let i = min; i <= max; i++) {
+        this.array.push(i);
+      }
+      this.left_limit = min;
+      this.right_limit = max;
     }
 
     guess() {
-
+      return this.array[Math.ceil((this.right_limit + this.left_limit) / 2)];
     }
 
     lower() {
-
+      return this.right_limit = Math.ceil((this.right_limit + this.left_limit) / 2);
+      return this;
     }
 
     greater() {
-
+      return this.left_limit = Math.ceil((this.right_limit + this.left_limit) / 2);
+      return this;
     }
 }
 
